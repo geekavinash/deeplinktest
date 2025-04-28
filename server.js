8e77e84f207334a27cb760748591c8fc70b332b7
geekavinash <avigeek@outlook.in>
2025-04-28 16:16:04 +0530

    final

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 443; // Change to 80 or 443 in production
+
+const content = {
+  applinks: {
+    apps: [],
+    details: [
+      {
+        appID: "QUMGU599JQ.in.goindigo.IndiGo.uat",
+        paths: ["*"],
+      },
+      {
+        appID: "QUMGU599JQ.in.goindigo.IndiGo.uat.debug",
+        paths: ["*"],
+      },
+    ],
+  },
+};
+
+app.get('/apple-app-site-association', (req, res) => {
+  res.setHeader('Content-Type', 'application/json'); // ✅ Correct Content-Type
+  res.status(200).send(JSON.stringify(content));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
