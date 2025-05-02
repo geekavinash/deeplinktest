b1f5c39c6cff955fcdc8df8d37af1d08ae342621
geekavinash <avigeek@outlook.in>
2025-05-02 17:05:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,42 +4,19 @@ const app = express();
 const port = 443; // 80 or 443 in production
 
 const content = {
-
-  "applinks": {
+    "applinks": {
+    "apps": [],
     "details": [
-      {
-        "appIDs": ["8MRL5ZLUXW.in.goindigo.IndiGo.preprod", "8MRL5ZLUXW.in.goindigo.IndiGo.preprod.debug"],
-        "components": [
-          {
-            "/": "/loyalty/*",
-            "exclude": true,
-            "comment": "Matches any URL with a path that starts with /loyalty or anything after it and instructs the system not to open it as a universal link."
-          },
-          {
-            "/": "hotels*",
-            "exclude": true,
-            "comment": "Matches any URL with a path that starts with /hotels or anything after it and instructs the system not to open it as a universal link."
-          },
-          {
-            "/": "/?ui-ux=oldui",
-            "exclude": true,
-            "comment": "Matches any URL with the query parameter ui-ux=oldui and excludes it from universal links."
-          },
-          {
-            "/": "/",
-            "exclude": false,
-            "comment": "."
-          }
-        ]
-      }
+        {
+            "appID": "8MRL5ZLUXW.in.goindigo.IndiGo.preprod",
+            "paths": ["*"]
+        },
+        {
+            "appID": "8MRL5ZLUXW.in.goindigo.IndiGo.preprod.debug",
+            "paths": ["*"]
+        }
     ]
-  },
-  "webcredentials": {
-    "apps": ["8MRL5ZLUXW.in.goindigo.IndiGo.preprod"]
-  },
-  "appclips": {
-    "apps": ["8MRL5ZLUXW.in.goindigo.IndiGo.preprod"]
-  }
+    }
 };
 
 // Serve at /apple-app-site-association
