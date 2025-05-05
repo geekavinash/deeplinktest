bab96a41aa351b85e3b0bd5b53b0f1917e5b74d8
geekavinash <avigeek@outlook.in>
2025-05-05 15:37:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 443; // 80 or 443 in production
 
-const content = {
+const contentPreprod = {
     "applinks": {
     "apps": [],
     "details": [
@@ -19,22 +19,68 @@ const content = {
     }
 };
 
+const contentUat = {
+
+  "applinks": {
+    "details": [
+      {
+        "appIDs": ["QUMGU599JQ.in.goindigo.IndiGo.uat", "QUMGU599JQ.in.goindigo.IndiGo.uat.debug"],
+        "components": [
+          {
+            "/": "/loyalty/*",
+            "exclude": true,
+            "comment": "Matches any URL with a path that starts with /loyalty or anything after it and instructs the system not to open it as a universal link."
+          },
+          {
+            "/": "hotels*",
+            "exclude": true,
+            "comment": "Matches any URL with a path that starts with /hotels or anything after it and instructs the system not to open it as a universal link."
+          },
+          {
+            "/": "/?ui-ux=oldui",
+            "exclude": true,
+            "comment": "Matches any URL with the query parameter ui-ux=oldui and excludes it from universal links."
+          },
+          {
+            "*": "*cid=metasearch|googleflights*",
+            "exclude": true,
+            "comment": "Excludes any URL with the keyword cid=metasearch|googleflights in the query parameters."
+          },
+          {
+            "*": "*cid=metasearch|skyscanner*",
+            "exclude": true,
+            "comment": "Excludes any URL with the keyword cid=metasearch|skyscanner in the query parameters."
+          },
+          {
+            "*": "*cid=metasearch|wego*",
+            "exclude": true,
+            "comment": "Excludes any URL with the keyword cid=metasearch|wego in the query parameters."
+          }
+        ]
+      }
+    ]
+  },
+  "webcredentials": {
+    "apps": ["QUMGU599JQ.in.goindigo.IndiGo.uat"]
+  },
+  "appclips": {
+    "apps": ["QUMGU599JQ.in.goindigo.IndiGo.uat.debug"]
+  }
+}
+
 // Serve at /apple-app-site-association
 app.get('/apple-app-site-association', (req, res) => {
   res.setHeader('Content-Type', 'application/json'); // Required
-  res.status(200).send(JSON.stringify(content));
+  res.status(200).send(JSON.stringify(contentUat));
 });
 
 // Serve at /.well-known/apple-app-site-association
 app.get('/.well-known/apple-app-site-association', (req, res) => {
   res.setHeader('Content-Type', 'application/json'); // Required
-  res.status(200).send(JSON.stringify(content));
+  res.status(200).send(JSON.stringify(contentUat));
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
